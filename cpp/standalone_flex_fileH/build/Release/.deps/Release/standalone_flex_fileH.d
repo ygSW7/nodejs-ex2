715c5593d485603162c480e1576a29de49affd04
Redhat <gyang@localhost.localdomain>
2018-04-13 18:13:29 +0100

    gcc recompile

---
+++ cpp/standalone_flex_fileH/build/Release/.deps/Release/standalone_flex_fileH.d
@@ -0,0 +1,1 @@
+cmd_Release/standalone_flex_fileH := g++ -pthread -rdynamic -m64  -o Release/standalone_flex_fileH -Wl,--start-group ./Release/obj.target/standalone_flex_fileH/mainH.o -Wl,--end-group 
